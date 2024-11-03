cedb27e76ec84e7350b836a8efe57d57056f073d
shafadeascr16 <shafadea11@gmail.com>
2024-11-03 22:38:22 +0700

    Unir 4 Cupcake

---
+++ app/src/androidTest/java/com/example/cupcake/ScreenAssertions.kt
@@ -0,0 +1,10 @@
+package com.example.cupcake
+
+package com.example.cupcake.test
+
+import androidx.navigation.NavController
+import org.junit.Assert
+
+fun NavController.assertCurrentRouteName(expectedRouteName: String) {
+    Assert.assertEquals(expectedRouteName, currentBackStackEntry?.destination?.route)
+}
